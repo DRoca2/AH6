9ab622edc2854c8d93bfa4cd934edeb056d4ad38
Roca <destiny.roca@fiserv.com>
2023-11-01 15:23:29 -0400

    Algo hour 4

--- Solution.java
+++ Solution.java
@@ -1,6 +1,7 @@
 import java.util.List;
 
 import javax.xml.transform.Source;
+import java.util.*;
 
 class Node<T> {
   T val;
@@ -14,11 +15,17 @@ class Node<T> {
 
 class Solution {
   public static List<String> linkedListValues(Node<String> head) {
-    // todo
-    return;
+    List<String> value = new ArrayList<>();
+    Node<String> currentNode = head;
+
+    while(currentNode != null){
+      value.add(currentNode.val);
+      currentNode = currentNode.next;
+    }
+     return value;
   }
 
-  public static void main() {
+  public static void main(String[] args) {
     Node<String> a = new Node<>("a");
     Node<String> b = new Node<>("b");
     Node<String> c = new Node<>("c");
@@ -31,5 +38,7 @@ class Solution {
 
     Solution.linkedListValues(a); 
     // -> [ "a", "b", "c", "d" ]
+
+    System.out.println(Solution.linkedListValues(a));
   }
 }
